9568c40e7ead9e757b6457c56f98a1fbfad94adc
David Fraser <davidf@sjsoft.com>
2012-10-02 21:30:22 +0200

    Declarative base and users ORM class

--- blogapp.py
+++ blogapp.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 
 import sqlalchemy
-sqlalchemy.__version__ 
+sqlalchemy.__version__
 
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///:memory:', echo=False)
 
 engine.execute("select 1").scalar()
+
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+
+    def __init__(self, name, fullname, password):
+        self.name = name
+        self.fullname = fullname
+        self.password = password
+
+    def __repr__(self):
+       return "<User('%s','%s', '%s')>" % (self.name, self.fullname, self.password)
+
+User.__table__ # doctest: +NORMALIZE_WHITESPACE
+
+User.__mapper__ # doctest: +ELLIPSIS
+
+Base.metadata.create_all(engine)
 
