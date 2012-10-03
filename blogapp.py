6a1d3a78b7ff936e5b0bb613b0aa47b3f098a87d
David Fraser <davidf@sjsoft.com>
2012-10-03 15:33:27 +0200

    Added many-to-many section

--- blogapp.py
+++ blogapp.py
@@ -323,3 +323,66 @@ session.query(Address).filter(
     Address.email_address.in_(['jack@google.com', 'j25@yahoo.com'])
 ).count()
 
+from sqlalchemy import Table, Text
+# association table
+post_keywords = Table('post_keywords', Base.metadata,
+    Column('post_id', Integer, ForeignKey('posts.id')),
+    Column('keyword_id', Integer, ForeignKey('keywords.id'))
+)
+
+class BlogPost(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    headline = Column(String(255), nullable=False)
+    body = Column(Text)
+
+    # many to many BlogPost<->Keyword
+    keywords = relationship('Keyword', secondary=post_keywords, backref='posts')
+
+    def __init__(self, headline, body, author):
+        self.author = author
+        self.headline = headline
+        self.body = body
+
+    def __repr__(self):
+        return "BlogPost(%r, %r, %r)" % (self.headline, self.body, self.author)
+
+class Keyword(Base):
+    __tablename__ = 'keywords'
+
+    id = Column(Integer, primary_key=True)
+    keyword = Column(String(50), nullable=False, unique=True)
+
+    def __init__(self, keyword):
+        self.keyword = keyword
+
+from sqlalchemy.orm import backref
+# "dynamic" loading relationship to User
+BlogPost.author = relationship(User, backref=backref('posts', lazy='dynamic'))
+
+Base.metadata.create_all(engine)
+
+wendy = session.query(User).\
+                filter_by(name='wendy').\
+                one()
+post = BlogPost("Wendy's Blog Post", "This is a test", wendy)
+session.add(post)
+
+post.keywords.append(Keyword('wendy'))
+post.keywords.append(Keyword('firstpost'))
+
+session.query(BlogPost).\
+            filter(BlogPost.keywords.any(keyword='firstpost')).\
+            all()
+
+session.query(BlogPost).\
+            filter(BlogPost.author==wendy).\
+            filter(BlogPost.keywords.any(keyword='firstpost')).\
+            all()
+
+wendy.posts.\
+        filter(BlogPost.keywords.any(keyword='firstpost')).\
+        all()
+
