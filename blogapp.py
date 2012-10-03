9e09ac2070c69778cffdc7c81216f9ce63d2a2e7
David Fraser <davidf@sjsoft.com>
2012-10-03 11:59:31 +0200

    Added class to demonstrate relationships

--- blogapp.py
+++ blogapp.py
@@ -166,3 +166,22 @@ session.query(func.count('*')).select_from(User).scalar()
 
 session.query(func.count(User.id)).scalar() #doctest: +NORMALIZE_WHITESPACE
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+class Address(Base):
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship("User", backref=backref('addresses', order_by=id))
+
+    def __init__(self, email_address):
+        self.email_address = email_address
+
+    def __repr__(self):
+        return "<Address('%s')>" % self.email_address
+
+Base.metadata.create_all(engine) # doctest: +NORMALIZE_WHITESPACE
+
