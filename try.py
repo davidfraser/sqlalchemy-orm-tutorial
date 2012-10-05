a4c3b079d4d33bc3a5bdb56dfd11c29b541a58ef
David Fraser <davidf@sjsoft.com>
2012-10-05 10:21:18 +0200

    Allow going backwards...

--- try.py
+++ try.py
@@ -4,9 +4,12 @@ import code
 import sys
 
 class ReadLineConsole(code.InteractiveConsole):
+    BACK_CHAR = 'Z'
     def __init__(self, locals=None, filename="<console>", histfile=None):
         code.InteractiveConsole.__init__(self, locals, filename)
         self.queue = open("blogapp.py").readlines()
+        self.queue_pos = 0
+        self.back_search = None
         try:
             import readline
         except ImportError:
@@ -21,10 +24,30 @@ class ReadLineConsole(code.InteractiveConsole):
             readline.parse_and_bind("tab: complete")
 
     def complete(self, text, state):
+        # print (text, self.queue_pos, self.back_search, state)
         if text == '':
             if state == 0:
-                return self.queue.pop(0).rstrip("\n")
+                if self.back_search is not None:
+                    self.queue_pos -= self.back_search + 1
+                    self.back_search = None
+                if self.queue_pos >= len(self.queue):
+                    return "# That's all, folks!"
+                line = self.queue[self.queue_pos]
+                self.queue_pos += 1
+                return line.rstrip("\n")
             return None
+        elif text == self.BACK_CHAR:
+            if state == 0:
+                self.back_search = 1 if self.back_search is None else self.back_search + 1
+                back_pos = self.queue_pos - self.back_search
+                back_line = self.queue[back_pos if back_pos >= 0 else 0]
+                return self.BACK_CHAR + "0" + back_line.rstrip("\n")
+            elif state == 1:
+                back_pos = self.queue_pos - self.back_search - 1
+                back_line = self.queue[back_pos if back_pos >= 0 else 0]
+                return self.BACK_CHAR + "1" + back_line.rstrip("\n")
+            return None
+        self.back_search = None
         return self.completer.complete(text, state)
         
 ldict = locals().copy()
